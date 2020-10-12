2ee96edb1fc2bcb6a7f8565207a3bc09a8d4b540
Cyrus Chan <cyruschan@hsu.edu.hk>
2020-10-12 16:48:47 +0800

    init

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React , {useState,useEffect} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {checkConnected} from './functions';
+import NoConnectionScreen from "./screens/NoConnectionScreen";
 
 export default function App() {
+  const [connectStatus,setConnectStatus] = useState(false)
+
+  checkConnected().then(res=>{
+    setConnectStatus(res)
+  })
+
+  const alertSomething = () =>{
+    alert('alert something')
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    connectStatus?
+    (<View style={styles.container}>
+      <Text>Open up App.js to start working on your: {connectStatus} app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </View>):(
+      <NoConnectionScreen onCheck={checkConnected}/>
+    )
   );
 }
 
